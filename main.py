fbd87b644ac11cf81833a593677118945520a194
TinNguyen <ductin16111@gmail.com>
2022-07-23 16:55:42 +0300

    add title, icon and timeframe for pygame window

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((400, 500))
 
+# Title and Icon
+pygame.display.set_caption("SNAKE")
+icon = pygame.image.load('snake.png')
+pygame.display.set_icon(icon)
+
+# Time object to influence time in Pygame
+clock = pygame.time.Clock()
+
 running = True
 while running:
     # draw all elements
@@ -13,3 +21,4 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    clock.tick(60)      # time-frame: number of time while loop runs per sec
