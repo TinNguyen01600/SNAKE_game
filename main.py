8145c9af3dd526c5ce8fc9a7244c388722015d84
TinNguyen <ductin16111@gmail.com>
2022-07-23 11:58:47 +0300

    initialize game loop

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((400, 500))
+
 running = True
 while running:
+    # draw all elements
     pygame.display.update()
+    # background color RGB
+    screen.fill((0,0,0))    # screen should be drawn first, below all others
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
