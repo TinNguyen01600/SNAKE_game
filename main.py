65d5b772c5d285977fc6e48b69aa2deb7290daeb
TinNguyen <ductin16111@gmail.com>
2022-07-24 12:53:41 +0300

    create rectangle object

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cgi import test
 import pygame
 
 pygame.init()
@@ -15,6 +16,11 @@ clock = pygame.time.Clock()
 test_surface = pygame.Surface((100, 200)) # size of surface
 test_surface.fill((0, 0, 250))
 
+# Rectangle object
+# test_rect = pygame.Rect((100, 200, 100, 100))  # (x, y, w, h)
+test_rect = test_surface.get_rect(center = (200, 250))
+# place the rect around surface and put it in the middle of screen
+
 running = True
 while running:
     # draw all elements
@@ -24,9 +30,16 @@ while running:
     screen.fill((175, 215, 70))
     # screen.fill(pygame.Color('gold'))   # predefined color
     
+    test_rect.right += 1
+    
     # background surface
-    screen.blit(test_surface, (200, 250))   # position of surface
+    # screen.blit(test_surface, (200, 250))   # position of surface
                                             # top-left of the surface    
+    screen.blit(test_surface, test_rect)
+    
+    # Draw rectangle
+    # pygame.draw.rect(screen, pygame.Color('red'), test_rect)
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
