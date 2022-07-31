15bffbc6e583c0b670973789e4ad838b8e4be81a
TinNguyen <ductin16111@gmail.com>
2022-07-31 13:27:11 +0300

    add codes for snake eating fruits

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ from pygame.math import Vector2
 
 class FRUIT:
     def __init__(self):
-        # create x and y position
-        self.x = random.randint(0, cell_no - 1)
-        self.y = random.randint(0, cell_no - 1)
-        self.pos = Vector2(self.x, self.y)
-        # draw a square
+        self.randomize()
     def draw_fruit(self):
         # create a rect
         fruit_rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size, cell_size, cell_size)
         # draw the rect
         pygame.draw.rect(screen, (126, 166, 114), fruit_rect)
+    def randomize(self):    # create a random new position for the fruit
+        self.x = random.randint(0, cell_no - 1)
+        self.y = random.randint(0, cell_no - 1)
+        self.pos = Vector2(self.x, self.y)
 
 class SNAKE:
     def __init__(self):
         self.body = [Vector2(5,10), Vector2(6,10), Vector2(7,10)]
         self.direction = Vector2(1, 0)
+        self.new_block = False
     def draw_snake(self):
         for block in self.body:
             body_rect = pygame.Rect(block.x * cell_size, block.y * cell_size, cell_size, cell_size)
@@ -27,9 +28,17 @@ class SNAKE:
     # The head is moved to a new block, each following block 
     # is moved to the position of the previous block used to be
     def move_snake(self):
-        body_copy = self.body[:-1]  # get all body except for the last block
-        body_copy.insert(0, body_copy[0] + self.direction)  # add a new block head and direction
-        self.body = body_copy[:]
+        if self.new_block == True:
+            body_copy = self.body[:]    # get all snake's body
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+            self.new_block = False
+        else:
+            body_copy = self.body[:-1]  # get all body except for the last block
+            body_copy.insert(0, body_copy[0] + self.direction)  # add a new block head and direction
+            self.body = body_copy[:]
+    def add_block(self):
+        self.new_block = True
 
 # This class contains snake, fruit and other game logic
 class MAIN:
@@ -38,11 +47,16 @@ class MAIN:
         self.fruit = FRUIT()
     def update(self):
         self.snake.move_snake()
+        self.check_collision()
     def draw_elements(self):
         self.fruit.draw_fruit()
         self.snake.draw_snake()
-        
-
+    def check_collision(self):
+        if self.fruit.pos == self.snake.body[0]:
+            # reposition the fruit
+            self.fruit.randomize()
+            # add another block to the snake
+            self.snake.add_block()
 pygame.init()
 cell_size = 30
 cell_no = 20
